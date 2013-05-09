cd9120355ac3868880eb126a645ea3629dcca194
ToreB <toreb@mesan.no>
2013-05-09 18:45:16 +0200

    Oppgave 1 - Hello PhoneGap

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -34,8 +34,6 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        beep();
-        vibrate();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
